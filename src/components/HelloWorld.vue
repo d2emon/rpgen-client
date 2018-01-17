fa974f696691049a2956a0d786206dd4afaf7651
Dmitry Kutsenko <d2emonium@gmail.com>
2018-01-17 16:40:21 +0300

    Load worlds

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <v-btn @click="loadWorlds">Text</v-btn>
         <blockquote>
           &#8220;First, solve the problem. Then, write the code.&#8221;
           <footer>
@@ -15,6 +16,36 @@
     </v-slide-y-transition>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  name: 'helloWorld',
+  methods: {
+    loadWorlds: function () {
+      var api = 'http://127.0.0.1:3000/worlds'
+      this.axios.get(api).then((response) => {
+        console.log(response.data)
+        alert('worlds')
+      })
+    }
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import VueAxios from 'vue-axios'
 
 Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
